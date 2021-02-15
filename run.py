1c57782610da7c9928d433c8918ed53f521c9cf9
Alex Radnaev <alexander.radnaev@gmail.com>
2021-02-15 00:41:32 +0000

    added counters

--- run.py
+++ run.py
@@ -19,14 +19,23 @@ def main(command_line, monitor_period_s=10):
     logging.info('Started watchdog with command line: {}. Monitor period (s): {}'.format(
         command_line, monitor_period_s))
     p = start_subprocess(command_line)
+    check_counter = 0
+    restart_counter = 0
+    up_counter = 0
     while True:
         check = p.poll()
+        check_counter += 1
         if check is not None:
-            logging.warning('Process is terminated. Restarting.')
+            restart_counter += 1
+            logging.warning('Process is terminated. Restart #{}. Last Uptime(h): {}'.format(
+                restart_counter, up_counter * monitor_period_s / (3600 * 24))
             p = start_subprocess(command_line)
+            up_counter = 0
         else:
+            up_counter += 1
             logging.info('Process is alive. poll returned: "{}"'.format(check))
-        logging.info('Sleeping for {} seconds.'.format(monitor_period_s))
+            logging.info('Sleeping for {} seconds. Check counter: {}, restart counter: {}, upttime: {}'.format(
+            monitor_period_s, check_counter, restart_counter, up_counter * monitor_period_s / (3600 * 24)))
         sleep(monitor_period_s)
 
 
